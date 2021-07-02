4357f0d74c37b038f9a6c0b9395c46e6f5fdbe0b
TimaOrazkulov <tima.orazkulov@gmail.com>
2021-07-02 12:13:26 +0600

    changes

---
+++ QrRestaurantMenuApp/Views/FullMenuTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  FullMenuTableViewCell.swift
+//  QrRestaurantMenuApp
+//
+//  Created by Temirlan Orazkulov on 02.07.2021.
+//
+
+import UIKit
+
+class FullMenuTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
